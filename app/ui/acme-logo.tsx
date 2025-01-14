3ac19755edc81329801d192b76db2d5e3e1116a7
Sergey <sayvix005@gmail.com>
2025-01-14 16:34:52 +0300

    fix logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,10 +3,14 @@ import { GlobeAltIcon } from '@heroicons/react/24/outline';
 export default function AcmeLogo() {
   return (
     <div
-      className={`flex flex-row items-center leading-none text-white`}
+      className={`flex flex-row items-center leading-none text-white space-x-2`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className="h-8 w-8 md:h-12 md:w-12 rotate-[15deg]" />
+      <div className='flex flex-row md:flex-col space-x-2 md:space-x-0'>
+        <p className="text-[24px] font-bold md:text-[22px]">КГТК</p>
+        <p className="text-[24px] md:text-[20px]">Support</p>
+      </div>
+      
     </div>
   );
 }
