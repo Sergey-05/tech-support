b3928b1caeaaaa97405f73b6047bacbeef08df65
Sergey <sayvix005@gmail.com>
2025-01-18 15:48:54 +0300

    fix logout

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,14 +1,13 @@
 // app/api/logout/route.ts
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { logout } from '@/app/lib/actions';
 import { redirect } from 'next/navigation';
 
 
-export async function POST() {
+export async function POST(req: Request) {
   try {
     await logout();
-    redirect('/login');
-    return NextResponse.json({ message: 'Logged out successfully' });
+    return NextResponse.redirect('/login')
   } catch (error) {
     return NextResponse.json({ error: 'Failed to log out' }, { status: 500 });
   }
